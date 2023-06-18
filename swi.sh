cf4b259f0151e396e1665487f8553aa78572dc82
Bryan Bennett <Bryan.Bennett@protonmail.com>
2023-06-18 14:23:32 -0400

    Disable gfm
    Enable the specific addons one-by-one,
    ignoring tagfilter

--- swi.sh
+++ swi.sh
@@ -89,7 +89,7 @@ swish_menu() {
 }
 
 swish_body() {
-    comrak --unsafe --gfm "$1" --syntax-highlighting none | minify --type html
+    comrak --unsafe --extension strikethrough,table,autolink,tasklist "$1" --syntax-highlighting none | minify --type html
 }
 
 swish_page() {
